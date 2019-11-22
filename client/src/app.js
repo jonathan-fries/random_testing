171b8354979ac17c88015d534293f0e0c5d783aa
Jonathan <jonathan.fries@gmail.com>
2019-11-22 15:27:29 -0700

    First commit, basic react structure.

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppContainer from './appcontainer';
+
+export default class App extends React.Component{
+
+
+    render(){
+        return <AppContainer/>;
+
+    }
+}
---
+++ client/src/appcontainer.js
@@ -0,0 +1,13 @@
+import './appcontainer.scss';
+import React from 'react';
+
+export default class AppContainer extends React.Component {
+
+    render() {
+        return <div className='contentBox'>
+                <h1>Title is here.</h1>
+                <p>Text is here.</p>
+                </div>;
+                {/* } <Footer/> */}
+    }
+}
