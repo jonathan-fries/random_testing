94b3c81e15d74539fa2fb3ae17eb6257a7fd6857
Jonathan <jonathan.fries@gmail.com>
2019-11-26 11:24:46 -0700

    Input box for the number of iterations to perform.

--- client/src/appcontainer.js
+++ client/src/appcontainer.js
@@ -3,13 +3,16 @@ import React from 'react';
 import Footer from './components/footer';
 import InputMin from './components/input_min';
 import InputMax from './components/input_max';
+import Iterations from './components/iterations';
 
 export default class AppContainer extends React.Component {
+
+
 
     render() {
         return <div className='contentBox'>
                 <h1>Testing Randomness</h1>
-                <InputMin/><InputMax/>
+                <InputMin/><InputMax/><Iterations/>
                 <Footer/>
                 </div>;
                 {/* } <Footer/> */}
