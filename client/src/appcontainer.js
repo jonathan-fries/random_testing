c1b79e8618a826eadb96a543ce3ae10a3968d38c
Jonathan <jonathan.fries@gmail.com>
2019-12-02 16:24:25 -0700

    Replacing individual inputs with form components for easier validation.

--- client/src/appcontainer.js
+++ client/src/appcontainer.js
@@ -1,20 +1,14 @@
 import './appcontainer.scss';
 import React from 'react';
 import Footer from './components/footer';
-import InputMin from './components/input_min';
-import InputMax from './components/input_max';
-import Iterations from './components/iterations';
-import Button from 'react-bootstrap/button';
+import RandomMain from './components/random_main';
 
 export default class AppContainer extends React.Component {
-
-
 
     render() {
         return <div className='contentBox'>
                 <h1>Testing Randomness</h1>
-                <InputMin/><InputMax/><Iterations/>
-                <Button variant="primary">Test</Button>
+                <RandomMain />
                 <Footer/>
                 </div>;
     }
