0139d4343d5b0c55fc1e2a7e09d26ac7de17b962
Jonathan <jonathan.fries@gmail.com>
2019-12-02 15:42:55 -0700

    Adding button.

--- client/src/appcontainer.js
+++ client/src/appcontainer.js
@@ -4,6 +4,7 @@ import Footer from './components/footer';
 import InputMin from './components/input_min';
 import InputMax from './components/input_max';
 import Iterations from './components/iterations';
+import Button from 'react-bootstrap/button';
 
 export default class AppContainer extends React.Component {
 
@@ -13,8 +14,8 @@ export default class AppContainer extends React.Component {
         return <div className='contentBox'>
                 <h1>Testing Randomness</h1>
                 <InputMin/><InputMax/><Iterations/>
+                <Button variant="primary">Test</Button>
                 <Footer/>
                 </div>;
-                {/* } <Footer/> */}
     }
 }
